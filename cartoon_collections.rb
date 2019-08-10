a16c7245df3fa85cf5b1d8faa4d1ed3eb2eed656
Dan Romans <danieljromans@gmail.com>
2019-08-10 16:30:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,11 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any? { |call| call.length > 4 }
-  array.all? { |call| call.length <= 4 }
+  if array.any? { |call| call.length > 4 }
+    return true
+  elsif
+    array.all? { |call| call.length <= 4 }
+    return false
 end
 
 def find_the_cheese# code an argument here
