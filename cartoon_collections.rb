5aaf1490eebcd612f011138318007f819d82c52e
Dan Romans <danieljromans@gmail.com>
2019-08-10 16:07:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,7 @@
 def roll_call_dwarves(array)
-  list = array
-  i = 0
-  while i < array.length do
     array.each_with_index do |item, index|
-    array[item] = index
-    i += 1
-    end
+    puts "#{index + 1}. #{item}"
   end
-  puts list
 end
 
 def summon_captain_planet# code an argument here
